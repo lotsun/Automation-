c9206358d14e7cde8529f6731e0763d3712c968b
Newland Lotsu <50834895+lotsun@users.noreply.github.com>
2023-11-24 19:28:45 +0000

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,12 @@
+# This program says hello and ask my name
+
+print("hello world!")
+print ("Whats my name ? ") #ask for their name
+myName = input()
+print("it is good to meet you," + myName)
+print("The length of your name is:")
+print(len(myName))
+print("What is your age ?")#ask for their age
+myAge = input()
+print("you will be" + str(int(myAge) + 1) + "" "in a year.")
+      
